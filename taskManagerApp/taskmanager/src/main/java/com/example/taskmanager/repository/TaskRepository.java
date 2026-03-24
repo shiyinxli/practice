65f7d5dd81b25a3cf25b18d8412eab07714b9675
Shiyin Li <lsyin85@gmail.com>
2026-03-24 15:57:53 +0100

    delete old taskmanager project folders

--- taskManagerApp/taskmanager/src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.taskmanager.repository;
-
-import com.example.taskmanager.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
